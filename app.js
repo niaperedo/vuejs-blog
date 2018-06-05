8224b78dabbcb735d9e877f9f686b0639fd8d344
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-06-05 14:36:22 +0800

    Add lesson 9

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 new Vue({
     el: "#vue-app",
     data: {
-        name: "",
-        age: ""
+        age: 20,
+        a: 0,
+        b: 0
     },
     methods: {
-       logName: function() {
-            console.log('You entered your name'); 
-       },
-       logAge: function() {
-            console.log('You entered your age');
-       }
-    }
-});  
+        
+    },
+    computed: {
+      addToA: function() {
+        console.log('addToA');
+        return this.a + this.age;
+      },
+      addToB: function() {
+        console.log('addToB');
+        return this.b + this.age;
+      }
+    } 
+});   
