7351d17f2b9a04a045077da33f4b89a93704f66a
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-06-05 11:24:15 +0800

    Add lesson 4

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: "#vue-app",
     data: {
          name: 'Shaun',
-         job: 'Ninja'
+         job: 'Ninja',
+         website: 'http://thenetninja.co.uk',
+         websiteTag:'<a href="http://thenetninja.co.uk">The Net Ninja</a>'
     },
     methods: {
         greet: function(time) {
