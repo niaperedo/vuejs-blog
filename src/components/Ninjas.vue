fc867de3db70eb8484b0907e52e9307dc72a7ab8
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-06-06 16:54:35 +0800

    Add lesson 22

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -12,16 +12,16 @@
 <script>
 
 export default {
+  // props: ['ninjas'],
+  props: {
+    ninjas: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
-      ninjas: [
-        {name: 'Ryu', specialty: 'Vue Components', show: false},
-        {name: 'Crystal', specialty: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', specialty: 'Click Events', show: false},
-        {name: 'Tango', specialty: 'Conditionals', show: false},
-        {name: 'Kami', specialty: 'Webpack', show: false},
-        {name: 'Yoshi', specialty: 'Data Diggin', show: false}
-      ]
+      
     }
   }
 }
