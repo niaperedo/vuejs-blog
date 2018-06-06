8c7a28a885c2d6ac47459b4768958848107e6b43
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-06-06 14:25:06 +0800

    Add lesson 18

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'Your first Vue file, wooo!'
+    }
+  },
+  methods: {
+      greeting() {
+        return 'heeeeey cowboy!';
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
