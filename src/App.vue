657ee0e81dd7cce26ff8a1c0329de4468d5ae481
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-06-07 13:22:23 +0800

    Add lesson 27

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,38 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <hr>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas> 
-    <app-footer v-bind:title="title"></app-footer> 
+    <form-helper>
+     <div slot="form-header">
+       <h3>This is the title of the form</h3>
+       <p>Information about the form</p>
+     </div>
+     <div slot="form-fields">
+       <input type="text" placeholder="name" required>
+       <input type="text" placeholder="password" required>
+     </div>
+     <div slot="form-controls">
+       <button v-on:click="handleSubmit ">Submit</button>
+     </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue'
+  
+import formHelper from './components/formHelper.vue'
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas,
+    'form-helper': formHelper
   },
   data() {
     return {
-      ninjas: [
-        {name: 'Ryu', specialty: 'Vue Components', show: false},
-        {name: 'Crystal', specialty: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', specialty: 'Click Events', show: false},
-        {name: 'Tango', specialty: 'Conditionals', show: false},
-        {name: 'Kami', specialty: 'Webpack', show: false},
-        {name: 'Yoshi', specialty: 'Data Diggin', show: false}
-      ],
-      title: "Vue Ninjas"
+      title: "I am a dynamic slot title"
     }
   },
   methods: {
-    updateTitle(updatedTitle) {
-      this.title = updatedTitle; 
-    }
+     handleSubmit() {
+      return {};
+     }
   }
 }
 </script>
