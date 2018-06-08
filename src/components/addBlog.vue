eecb3e633f3fece77680c55cb564968f3ec55709
Adrianne Peredo <aperedo@uth702@gmail.com>
2018-06-08 15:48:11 +0800

    Add Lesson 43

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -58,11 +58,7 @@ export default {
   },
   methods: {
     post() {
-        this.$http.post('https://jsonplaceholder.typicode.com/posts', {
-            title: this.blog.title,
-            body: this.blog.content,
-            userId: 1
-        }).then(function(data) {
+        this.$http.post('https://vuejs-tutorial-13d67.firebaseio.com/posts.json', this.blog).then(function(data) {
             console.log(data);
             this.submitted = true;
         });
